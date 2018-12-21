4758e4321c8b7b6b47866028166ab3748d4308c6
St1llman <dm1korneev@gmail.com>
2018-12-21 13:14:27 +0300

    move to angular SPA

--- app.js
+++ app.js
@@ -9,23 +9,12 @@ require("./app_api/models/db");
 var uglifyJs = require("uglify-js");
 var fs = require("fs");
 
-var routes = require("./app_server/routes/index");
-var apiRoutes = require("./app_api/routes/index");
-
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 app.set("view engine", "jade");
 
-var appClientsFiles = [
-  "app_client/app.js",
-  "app_client/home/home.controller.js",
-  "app_client/common/services/coffeToGoData.service.js",
-  "app_client/common/services/geolocation.service.js",
-  "app_client/common/filters/formatDistance.filter.js",
-  "app_client/common/directives/ratingStars/ratingStars.directive.js"
-];
 var uglified = uglifyJs.minify(
   {
     "app.js": fs.readFileSync("app_client/app.js", "utf8"),
@@ -67,8 +56,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "app_client")));
 
-app.use("/", routes);
-app.use("/api", apiRoutes);
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "app_client", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
