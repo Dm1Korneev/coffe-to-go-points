f8dd4316f025eb878e88e50ec1445bd5cee0aeda
St1llman <dm1korneev@gmail.com>
2018-12-21 10:56:40 +0300

    fix distance

--- app_client/common/services/coffeToGoData.service.js
+++ app_client/common/services/coffeToGoData.service.js
@@ -5,7 +5,7 @@
   function coffeToGoData($http) {
     locationsByCoords = function(lng, lat) {
       return $http.get(
-        "/api/locations?lng=" + lng + "&lat=" + lat + "&maxDistance=2000000"
+        "/api/locations?lng=" + lng + "&lat=" + lat + "&maxDistance=2000"
       );
     };
     return { locationsByCoords: locationsByCoords };
